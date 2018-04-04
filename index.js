be1031e99e4e8cb53f225f1c0a24950ae1d0d8ce
norman lewis <takialog@gmail.com>
2018-04-04 16:03:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,36 +13,17 @@ class President {
   passBill() {
     return 'You can do that!'
   }
-    
+
+ doCharity() {
+    returns "I like to help people."
+ }
+
+ conductPressInterview() {
+   returns "I am proud to be an American."
+ }
+
+  sauyHi() {
+    returns `Hi, my name is ${this.name}, I am from ${this.homestate}. I represent the ${this.politcalParty}s and was in office <yearsInOffice>.`
+  }
 }
- 
-//   describe('veto()', function() {
-//     it('returns "NO!"', function() {
-//       expect(lyndonJohnson.veto()).toEqual("NO!")
-//     })
-//   })
-//
-//   describe('passBill()', function() {
-//     it('returns "You can do that!"', function() {
-//       expect(lyndonJohnson.passBill()).toEqual("You can do that!")
-//     })
-//   })
-//
-//   describe('doCharity()', function() {
-//     it('returns "I like to help people."', function() {
-//       expect(lyndonJohnson.doCharity()).toEqual("I like to help people.")
-//     })
-//   })
-//
-//   describe('conductPressInterview()', function() {
-//     it('returns "I am proud to be an American."', function() {
-//       expect(lyndonJohnson.conductPressInterview()).toEqual("I am proud to be an American.")
-//     })
-//   })
-//
-//   describe('sayHi()', function() {
-//     it('returns "Hi, my name is <name>, I am from <homestate>. I represent the <politcalParty>s and was in office <yearsInOffice>."', function() {
-//       expect(lyndonJohnson.sayHi()).toEqual("Hi, my name is Lyndon B Johnson. I am from Texas. I represent the Democrats, and was in office 1963-1969.")
-//     })
-//   })
-// })
+
