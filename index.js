4a6c962c093dd6428dbd9724fe5fb28da3ceea82
norman lewis <takialog@gmail.com>
2018-04-04 16:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ class President {
   }
 
  doCharity() {
-    returns "I like to help people."
+    return "I like to help people."
  }
 
  conductPressInterview() {
-   returns "I am proud to be an American."
+   return "I am proud to be an American."
  }
 
   sauyHi() {
-    returns `Hi, my name is ${this.name}, I am from ${this.homestate}. I represent the ${this.politcalParty}s and was in office <yearsInOffice>.`
+    return `Hi, my name is ${this.name}, I am from ${this.homestate}. I represent the ${this.politcalParty}s and was in office <yearsInOffice>.`
   }
 }
