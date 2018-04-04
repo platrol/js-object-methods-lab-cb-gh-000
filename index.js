7d3caf0210d69d8e6375c72369d11a10b2d853bf
norman lewis <takialog@gmail.com>
2018-04-04 15:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class President {
 //   before(() => {
 //     lyndonJohnson = new President("Lyndon B Johnson", "Democrat", "1963-1969", "Texas")
 //   })
-// 
+//
 //   describe('Presidents Constructor Function', function() {
 //     it('can create a president with a name, home state, political party, and years in office', function() {
 //       expect(lyndonJohnson).toBeA(President)
@@ -32,25 +32,25 @@ class President {
 //       expect(lyndonJohnson.veto()).toEqual("NO!")
 //     })
 //   })
-// 
+//
 //   describe('passBill()', function() {
 //     it('returns "You can do that!"', function() {
 //       expect(lyndonJohnson.passBill()).toEqual("You can do that!")
 //     })
 //   })
-// 
+//
 //   describe('doCharity()', function() {
 //     it('returns "I like to help people."', function() {
 //       expect(lyndonJohnson.doCharity()).toEqual("I like to help people.")
 //     })
 //   })
-// 
+//
 //   describe('conductPressInterview()', function() {
 //     it('returns "I am proud to be an American."', function() {
 //       expect(lyndonJohnson.conductPressInterview()).toEqual("I am proud to be an American.")
 //     })
 //   })
-// 
+//
 //   describe('sayHi()', function() {
 //     it('returns "Hi, my name is <name>, I am from <homestate>. I represent the <politcalParty>s and was in office <yearsInOffice>."', function() {
 //       expect(lyndonJohnson.sayHi()).toEqual("Hi, my name is Lyndon B Johnson. I am from Texas. I represent the Democrats, and was in office 1963-1969.")
